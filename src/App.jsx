1486817a8a89fb2356dd628b88b05947ac6ec9d3
Finisus <finisvisus@gmail.com>
2024-08-17 15:31:36 +0530

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import pfpBanner from './assets/pfp.png';
 
 function App() {
   return (
     <>
+      <Header />
       <img src={pfpBanner} alt='Holy Shit!' />
       <Footer />
     </>
